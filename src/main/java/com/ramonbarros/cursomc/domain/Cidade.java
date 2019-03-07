0436c72ec2d016b6dd9cb82614a91896d9f5c861
Ramon Barros <ramonrpb@hotmail.com>
2019-03-07 13:48:27 -0300

    Endpoint /clientes/{id}

--- src/main/java/com/ramonbarros/cursomc/domain/Cidade.java
+++ src/main/java/com/ramonbarros/cursomc/domain/Cidade.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable{
 	
@@ -19,6 +21,7 @@ private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
---
+++ src/main/java/com/ramonbarros/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.ramonbarros.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import com.ramonbarros.cursomc.domain.Cliente;
+import com.ramonbarros.cursomc.repositories.ClienteRepository;
+import com.ramonbarros.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = ((CrudRepository<Cliente, Integer>) repo).findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
