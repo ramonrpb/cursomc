4aa21c0e1ed17de14f30d9ba617817c164a6d3d3
ramonrpb <ramonrpb@hotmail.com>
2019-10-17 14:00:26 -0300

    Implementando SmtpEmailService com servidor do Google.

--- src/main/java/com/ramonbarros/cursomc/config/DevConfig.java
+++ src/main/java/com/ramonbarros/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ramonbarros.cursomc.services.DBService;
+import com.ramonbarros.cursomc.services.EmailService;
+import com.ramonbarros.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -26,5 +28,10 @@ public class DevConfig {
 			dbService.instantiateTestDatabase();
 		}
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/ramonbarros/cursomc/services/DBService.java
+++ src/main/java/com/ramonbarros/cursomc/services/DBService.java
@@ -123,7 +123,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 =new Cliente(null, "Maria Silva", "maria@gmail.com", "67883210785", TipoCliente.PESSOAFISICA);
+		Cliente cli1 =new Cliente(null, "Maria Silva", "ramonrpb@hotmail.com", "67883210785", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("23145366", "23145538"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
