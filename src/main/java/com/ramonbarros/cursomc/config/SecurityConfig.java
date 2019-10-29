7e2403369fd03c774211433b9e9d1e637f3c02f7
ramonrpb <ramonrpb@hotmail.com>
2019-10-29 15:44:17 -0300

    Correção de erro na Autorização/Autenticação.

--- src/main/java/com/ramonbarros/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ramonbarros/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.ramonbarros.cursomc.security.JWTAuthenticationFilter;
 import com.ramonbarros.cursomc.security.JWTAuthorizationFilter;
 import com.ramonbarros.cursomc.security.JWTUtil;
 
@@ -62,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
